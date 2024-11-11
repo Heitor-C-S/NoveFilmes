3893202c3179311c75c9657a1de6a2c0122b8476
Heitor-C-S <heitorcsantana.1811@gmail.com>
2024-11-11 14:14:08 -0300

    commit inicial

---
+++ novefilmes/src/App.tsx
@@ -0,0 +1,13 @@
+const App = () =>{
+  return(
+  <>
+    <div>
+      <img src="./../public/logo-nove.svg"/>
+      <h1>Nove Filmes</h1>
+      <h3>Câmera, luz e maquinário</h3>
+    </div>
+  </>
+  )
+}
+
+export default App;
